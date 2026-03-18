0c32e1d73022fec33df73a194c5b092ad987aaca
Hoàng Trang <trang010280@gmail.com>
2026-03-18 16:11:05 +0700

    feat(auth): implement JWT login with email and password

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,19 @@ declare const module: any;
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from 'src/global/filters/all-exception.filter';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
-  app.useGlobalFilters(new AllExceptionFilter())
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalFilters(new AllExceptionFilter());
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../../../prisma/prisma.module'; 
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController]
+})
+export class AuthModule {}
