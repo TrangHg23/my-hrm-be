fb8f3664d859629af93ea387783556c96e0d3a9e
Hoàng Trang <trang010280@gmail.com>
2026-03-27 10:38:08 +0700

    fix: allow cors for vercel domain

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://my-hrm-one.vercel.app'],
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
